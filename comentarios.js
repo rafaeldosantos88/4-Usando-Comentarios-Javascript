5cb39d90f8b185e1d72978499ec9829e1d3a4cc5
Rafael Dos Santos <raphael_979@hotmail.com>
2019-11-20 22:42:07 -0300

    Usando comentarios no JavaScript

---
+++ comentarios.js
@@ -0,0 +1,8 @@
+//Escrever um comentário
+console.log('ola Mundo');
+console.log('linha1');
+console.log('linha2');
+console.log('linha3');
+//console.log('linha4');
+//vai ser ignorado a linha 4.Podemos usar bloco de codigo /* */ no caso pega todas as linhas do motor do Javascript
+console.log('linha5');
